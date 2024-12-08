edf502c2697573cc709e1a71b52cd621c9a0c14a
d3dfx <evnlnkfrd@gmail.com>
2024-12-07 19:21:49 -0500

    +2024 day 1 part 1 working

--- 2024/one/one.go
+++ 2024/one/one.go
@@ -2,6 +2,10 @@ package one
 
 import (
 	"fmt"
+	// "sort"
+	"math"
+	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -17,23 +21,33 @@ func RunPart(part string, puzzleInput string) {
 
 func partOne(input string) string {
 	a, b := split_loc_lists(input)
-	fmt.Println(a)
-	fmt.Println(b)
-	return ""
+
+	slices.Sort(a)
+	slices.Sort(b)
+
+	total_dist := 0
+	for idx := 0; idx < len(a); idx++ {
+		total_dist = total_dist + int(math.Abs((float64(a[idx] - b[idx]))))
+	}
+
+	return strconv.Itoa(total_dist)
 }
 
 func partTwo(input string) string {
 	return ""
 }
 
-func split_loc_lists(loc_lists string) (loc_list1, loc_list2 []string) {
+func split_loc_lists(loc_lists string) (loc_list1, loc_list2 []int) {
+
 	for _, loc := range strings.Split(loc_lists, "\n") {
-		var loc_list1 []string
-		var loc_list2 []string
 		tmp1, tmp2, found := strings.Cut(loc, "   ")
+
+		left_elem, _ := strconv.Atoi(tmp1)
+		right_elem, _ := strconv.Atoi(tmp2)
+
 		if found {
-			loc_list1 = append(loc_list1, tmp1)
-			loc_list2 = append(loc_list2, tmp2)
+			loc_list1 = append(loc_list1, left_elem)
+			loc_list2 = append(loc_list2, right_elem)
 		}
 	}
 	return loc_list1, loc_list2
